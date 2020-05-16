4d901ae80d341e9ca5cad228b72ed6dae9eee351
guptdivy <divyanshg10@gmail.com>
2020-05-16 19:52:07 +0530

    Updates

---
+++ precache-manifest.74235bfedf7e4256d31966f7fbb88c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d98551740d406e729325e2abd86327",
+    "url": "/react-profile/index.html"
+  },
+  {
+    "revision": "65e09f20637835c5c1d2",
+    "url": "/react-profile/static/css/main.3a8a4896.chunk.css"
+  },
+  {
+    "revision": "3115609d65286d270ff3",
+    "url": "/react-profile/static/js/2.968f4e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-profile/static/js/2.968f4e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e09f20637835c5c1d2",
+    "url": "/react-profile/static/js/main.64add8b0.chunk.js"
+  },
+  {
+    "revision": "0c38b640dec2a3c5549e",
+    "url": "/react-profile/static/js/runtime-main.b86f37a3.js"
+  }
+]);
