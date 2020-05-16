ffa0bd262213791517a2cbe7d0a8c79c3c1e5ee6
guptdivy <divyanshg10@gmail.com>
2020-05-16 19:44:23 +0530

    everything done except color selection

---
+++ src/helpers/constants.js
@@ -0,0 +1,23 @@
+export const help= `<pre>Only following commands are accepted : 
+    pwd : for knowing the present working directory
+    ls [path]: for listing files and directories
+    cd path: for changing directory
+    cat filename : for display the file
+    help : for details for accepted commands</pre>`;
+
+
+export const projects = `<pre>You can view all my projects <a href="https://github.com/guptdivy?tab=repositories">here</a><pre>`;
+
+export const personalInfo =`<pre>My name is Divyansh Gupta.
+My hometown is Agra.
+Currently, I am pursueing B.Tech. in computer science
+from National Institute of Technology, Patna<pre>`;
+
+export const education =`<pre>Currently pursueing B.tech. in CS from NIT Patna.
+I will be graduating in mid 2022.
+I did my 10<sup>th</sup> and 12<sup>th</sup> from <a href="https://standrewsschoolagra.com">St Andrews School</a>,
+Agra with 10 C.G.P.A. and 98.2% respectively.</pre>`;
+
+export const socialMediaHandles =`<pre>Linkedin : <a href="https://www.linkedin.com/in/divyansh-gupta-248621195/">Divyansh Gupta</a>
+Instagram : <a href="https://www.instagram.com/divyanshg10/">divyanshg10</a>
+Sorry, not on facebook.</pre>`;
